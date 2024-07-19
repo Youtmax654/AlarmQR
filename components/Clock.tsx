077c188f7d5dc744ca1097cec05cc5beef852da4
Youtmax654 <pennmaxime@gmail.com>
2024-07-19 08:08:15 +0200

    style: creating the clock frame with neumorph style.

--- components/Clock.tsx
+++ components/Clock.tsx
@@ -1,11 +1,48 @@
-import { Text, View } from "react-native";
+import { Canvas, Circle, Group, Shadow } from "@shopify/react-native-skia";
+import { useState } from "react";
+import { LayoutChangeEvent, Text } from "react-native";
 
 type Props = {};
-
 export const Clock = ({}: Props) => {
   return (
-    <View className="w-[203px] h-[203px] my-12 mx-20 bg-slate-500">
-      <Text>Time: 13:00</Text>
-    </View>
+    <ClockFrame>
+      <Text>Salut</Text>
+    </ClockFrame>
+  );
+};
+
+type ClockFrameProps = {
+  children: React.ReactNode;
+};
+const ClockFrame = ({ children }: ClockFrameProps) => {
+  const [canvasDimensions, setCanvasDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const handleLayout = (event: LayoutChangeEvent) => {
+    const { width, height } = event.nativeEvent.layout;
+    setCanvasDimensions({ width, height });
+  };
+
+  const { width, height } = canvasDimensions;
+  const borderR = 101;
+  const containerR = 88;
+  const cx = width / 2;
+  const cy = height / 2;
+
+  return (
+    <Canvas style={{ flex: 1, width: "100%" }} onLayout={handleLayout}>
+      <Group>
+        <Shadow dx={-1} dy={-1} blur={1} color="#FFFFFF" />
+        <Shadow dx={12} dy={12} blur={8} color="#A6B4C8" />
+        <Circle cx={cx} cy={cy} r={borderR} color="#EEF0F5" />
+      </Group>
+      <Group>
+        <Shadow dx={12} dy={12} blur={8} color="#A6B4C8" inner />
+        <Shadow dx={-1} dy={-1} blur={1} color="#FFFFFF" inner />
+        <Circle cx={cx} cy={cy} r={containerR} color="#EEF0F5" />
+      </Group>
+    </Canvas>
   );
 };

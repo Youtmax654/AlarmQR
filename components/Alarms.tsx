9e5d2ac92c25e7617c01d4ff1f80e1eb139b0c25
Youtmax654 <pennmaxime@gmail.com>
2024-07-22 19:39:39 +0200

    feat: Improve Alarms component UI and functionality.
    This commit updates the Alarms component to improve its UI and functionality. It introduces a new design using the @shopify/react-native-skia library for rendering a rounded title bar with a shadow effect. Additionally, it adds a new AlarmTopTitle component that displays the title "Alarms" on the title bar. The AlarmList component is also updated to use a ScrollView for better scrolling behavior and to display a list of alarms with a slider for toggling their active state. These changes enhance the overall user experience and make the Alarms feature more visually appealing and user-friendly.

--- components/Alarms.tsx
+++ components/Alarms.tsx
@@ -1,11 +1,80 @@
 import { Entypo } from "@expo/vector-icons";
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
-import { Pressable, ScrollView, Text, View } from "react-native";
+import {
+  Canvas,
+  Group,
+  RoundedRect,
+  Shadow,
+  Text,
+  useFont,
+} from "@shopify/react-native-skia";
+import { useState } from "react";
+import {
+  LayoutChangeEvent,
+  Pressable,
+  Text as RNText,
+  ScrollView,
+  View,
+} from "react-native";
 import { useAlarmStore } from "../hooks/useAlarmStore";
 import { Alarm } from "../utils/alarm";
 
 type Props = {};
 export const Alarms = ({}: Props) => {
+  return (
+    <View className="w-full items-center bg-alarms-light flex-1 rounded-[20px]">
+      <AlarmTopTitle />
+      <AlarmList />
+    </View>
+  );
+};
+
+const AlarmTopTitle = () => {
+  const [canvasDimensions, setCanvasDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
+  const { width } = canvasDimensions;
+
+  const font = useFont(
+    require("../assets/fonts/poppins/Poppins-Medium.ttf"),
+    18
+  );
+
+  const handleLayout = (event: LayoutChangeEvent) => {
+    const { width, height } = event.nativeEvent.layout;
+    setCanvasDimensions({ width, height });
+  };
+
+  return (
+    <>
+      <Canvas style={{ height: 80, width: "100%" }} onLayout={handleLayout}>
+        <Group>
+          <RoundedRect
+            x={0}
+            y={1}
+            height={50}
+            width={width}
+            r={20}
+            color="#EAECF1"
+          />
+          <Shadow dx={-1} dy={-1} blur={1} color="#FFFFFF" />
+          <Text x={50} y={50} text="Alarms" font={font} color="#646E82" />
+        </Group>
+      </Canvas>
+      <Pressable
+        className="absolute right-10 top-9"
+        onPress={() => {
+          alert("Options button");
+        }}
+      >
+        <Entypo name="dots-three-horizontal" size={24} color="#646E82" />
+      </Pressable>
+    </>
+  );
+};
+
+const AlarmList = () => {
   const { alarms, setAlarms } = useAlarmStore();
 
   const handleAlarmToggle = (id: string) => {
@@ -41,61 +110,50 @@ export const Alarms = ({}: Props) => {
     });
   };
 
-  return (
-    <View className="w-full items-center bg-slate-500 flex-1">
-      <View className="flex-row justify-between items-center w-full px-12 py-6">
-        <Text className="text-lg font-medium">Alarms</Text>
-        <Entypo name="dots-three-horizontal" size={24} color="black" />
-      </View>
-
-      <ScrollView
-        className="bg-slate-500 w-full h-full"
-        contentContainerStyle={{
-          alignItems: "center",
-          gap: 30,
-          paddingBottom: 30,
-        }}
-      >
-        {alarms.length > 0 ? (
-          alarms.map((alarm) => (
-            <Pressable
-              key={alarm.id}
-              onPress={() => handleAlarmClick(alarm.id)}
-              className="w-[88%] h-[75px] bg-slate-400 flex-row items-center justify-between px-4 rounded-[20px]"
-            >
-              <Text className="text-3xl font-medium">
-                {alarm.hour.toString().padStart(2, "0") +
-                  ":" +
-                  alarm.minute.toString().padStart(2, "0")}
-              </Text>
-              <Slider alarm={alarm} handler={handleAlarmToggle} />
-            </Pressable>
-          ))
-        ) : (
-          <Text className="text-lg font-medium">No alarms here</Text>
-        )}
-      </ScrollView>
-    </View>
-  );
-};
-
-type SliderProps = {
-  alarm: Alarm;
-  handler: (id: string) => void;
-};
-const Slider = ({ alarm, handler }: SliderProps) => {
-  return (
-    <Pressable
-      className={`relative w-12 h-6 rounded-full flex justify-center ${
-        alarm.active ? "bg-primary" : "bg-slate-500"
-      }`}
-      onPress={() => handler(alarm.id)}
-    >
-      <View
-        className={`w-7 h-7 absolute bg-slate-300 rounded-full ${
-          alarm.active ? "right-0" : "left-0"
+  const Slider = ({ alarm }: { alarm: Alarm }) => {
+    return (
+      <Pressable
+        className={`relative w-12 h-6 rounded-full flex justify-center ${
+          alarm.active ? "bg-primary" : "bg-slate-500"
         }`}
-      ></View>
-    </Pressable>
+        onPress={() => handleAlarmToggle(alarm.id)}
+      >
+        <View
+          className={`w-7 h-7 absolute bg-slate-300 rounded-full ${
+            alarm.active ? "right-0" : "left-0"
+          }`}
+        ></View>
+      </Pressable>
+    );
+  };
+
+  return (
+    <ScrollView
+      className="w-full h-full"
+      contentContainerStyle={{
+        alignItems: "center",
+        gap: 30,
+        paddingBottom: 30,
+      }}
+    >
+      {alarms.length > 0 ? (
+        alarms.map((alarm) => (
+          <Pressable
+            key={alarm.id}
+            onPress={() => handleAlarmClick(alarm.id)}
+            className="w-[88%] h-[75px] bg-slate-400 flex-row items-center justify-between px-4 rounded-[20px]"
+          >
+            <RNText className="text-3xl font-medium">
+              {alarm.hour.toString().padStart(2, "0") +
+                ":" +
+                alarm.minute.toString().padStart(2, "0")}
+            </RNText>
+            <Slider alarm={alarm} />
+          </Pressable>
+        ))
+      ) : (
+        <RNText className="text-lg font-medium">No alarms here</RNText>
+      )}
+    </ScrollView>
   );
 };

4aca898a0f7f152c395f85c66566eb027ee22185
Youtmax654 <pennmaxime@gmail.com>
2024-07-23 19:55:09 +0200

    refactor: Update NewAlarmBtn component to use @shopify/react-native-skia for rendering a custom button
    This commit updates the NewAlarmBtn component to use the @shopify/react-native-skia library for rendering a custom button with a circular shape and gradient background. The component now uses the Canvas, Circle, Group, LinearGradient, Shadow, and Text components from the library to create the desired visual effect. Additionally, the component now uses the useFont hook to load the Poppins-Medium font and display the "+" symbol in the center of the button. This change enhances the visual appeal of the NewAlarmBtn component and improves the overall user experience.

--- components/NewAlarmBtn.tsx
+++ components/NewAlarmBtn.tsx
@@ -1,10 +1,24 @@
 import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
-import { Pressable, Text, View } from "react-native";
+import {
+  Canvas,
+  Circle,
+  Group,
+  LinearGradient,
+  Shadow,
+  Text,
+  useFont,
+} from "@shopify/react-native-skia";
+import { Pressable } from "react-native";
 import { useAlarmStore } from "../hooks/useAlarmStore";
 
 type Props = {};
 export const NewAlarmBtn = ({}: Props) => {
   const { alarms, addAlarm } = useAlarmStore();
+
+  const font = useFont(
+    require("../assets/fonts/poppins/Poppins-Medium.ttf"),
+    36
+  );
 
   const handleNewAlarm = async () => {
     DateTimePickerAndroid.open({
@@ -22,13 +36,46 @@ export const NewAlarmBtn = ({}: Props) => {
   };
 
   return (
-    <View className="m-8 absolute bottom-8">
-      <Pressable
-        className="w-16 h-16 rounded-full bg-primary items-center justify-center"
-        onPress={handleNewAlarm}
-      >
-        <Text className="text-white font-bold text-4xl">+</Text>
-      </Pressable>
-    </View>
+    <Pressable onPress={handleNewAlarm} className="absolute bottom-8">
+      <Canvas style={{ height: 170, width: 200 }}>
+        <Group>
+          <Circle cx={100} cy={100} r={45} />
+          <LinearGradient
+            colors={["#E4E8F1", "#E6E9EF"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 150, y: 150 }}
+          />
+          <Shadow dx={5} dy={5} blur={5} color="rgba(0, 0, 0, 0.25)" />
+          <Shadow dx={-5} dy={-5} blur={5} color="rgba(255, 255, 255, 0.25)" />
+        </Group>
+        <Group>
+          <Circle cx={100} cy={100} r={35} />
+          <LinearGradient
+            colors={["#A6B4C8", "#768FB1"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 150, y: 150 }}
+          />
+        </Group>
+        <Group>
+          <Circle cx={100} cy={100} r={33} />
+          <LinearGradient
+            colors={["#FFA294", "#E5120A"]}
+            start={{ x: 70, y: 80 }}
+            end={{ x: 150, y: 150 }}
+          />
+          <Shadow dx={5} dy={5} blur={7} color="rgba(253, 37, 30, 0.35)" />
+          <Shadow dx={-5} dy={-5} blur={7} color="rgba(255, 255, 255, 0.6)" />
+        </Group>
+        <Group>
+          <Circle cx={100} cy={100} r={30} />
+          <LinearGradient
+            colors={["#FD251E", "#FF907E"]}
+            start={{ x: 70, y: 70 }}
+            end={{ x: 110, y: 110 }}
+          />
+        </Group>
+        <Text x={87} y={112} text="+" color="#FFFFFF" font={font} />
+      </Canvas>
+    </Pressable>
   );
 };
